41d048b183db746333cebf775ff8fea2c82b4043
WHYSTRIV3 <117491501+WHYSTRIV3@users.noreply.github.com>
2022-11-10 18:15:37 -0500

    Create Anime Power Tycoon.lua

---
+++ Anime Power Tycoon.lua
@@ -0,0 +1,222 @@
+local library = loadstring(game:HttpGet("https://gitlab.com/Ner0ox/versus/-/raw/main/ui/UIV2.lua", true))()
+local ui = library:CreateWindow("Anime Power Tycoon")
+
+local Main = ui:new("Main")
+local Misc = ui:new("Misc")
+
+local VU = game:GetService("VirtualUser")
+local Player = game:GetService("Players").LocalPlayer
+local WS = game:GetService("Workspace")
+local RS = game:GetService("ReplicatedStorage")
+local T = WS.Terrain
+local LI = game:GetService("Lighting")
+local Codes = {"Release", "Alien", "500Likes", "3kLikes", "BeastPet349", "BeastPet491", "BeastPet550"}
+--Variables
+local SelectedTycoon;
+local SelectedSpawns;
+
+-- Anti Afk
+
+Player.Idled:Connect(function()
+    VU:Button2Down(Vector2.new(0, 0), WS.CurrentCamera.CFrame)
+    wait(1)
+    VU:Button2Up(Vector2.new(0, 0), WS.CurrentCamera.CFrame)
+end)
+
+
+
+
+
+
+--functions
+
+function getTycoons()
+    local Tycoons = {}
+
+    for _,v in pairs(WS.tycoon1.Tycoons:GetChildren()) do
+            table.insert(Tycoons, v.Name)    
+    end
+    return Tycoons
+end
+
+
+
+
+
+
+
+
+
+
+
+
+--Main
+
+Main:CreateDropdown("Select Tycoon", getTycoons(), function(Tycoon)
+    SelectedTycoon = Tycoon
+
+end)
+
+
+Main:CreateButton("Teleport To Tycoon", function()
+    if SelectedTycoon then
+        Player.Character.HumanoidRootPart.CFrame = WS.tycoon1.Tycoons:FindFirstChild(SelectedTycoon).Spawn.CFrame
+    end
+end)
+
+Main:CreateToggle("Auto Collect", true, function()
+    if SelectedTycoon then
+        local giver = WS.tycoon1.Tycoons[SelectedTycoon].Essentials.Collector1.Giver
+        giver.Size = Vector3.new(.1, .1, .1)
+        giver.CanCollide = false
+        giver.CFrame = Player.Character.HumanoidRootPart.CFrame
+        task.wait(0.1)
+        giver.CFrame = Player.Character.HumanoidRootPart.CFrame * CFrame.new(-1000, -1000, 10000000)
+    end
+end)
+
+Main:CreateToggle("Auto Buy", true, function()
+    for _,v in pairs(WS.tycoon1.Tycoons[SelectedTycoon].Buttons:GetChildren()) do
+        if not v.Buton:FindFirstChild("Resim") then
+       v.Buton.Size = Vector3.new(.1, .1, .1)
+        v.Buton.CanCollide = false
+        v.Buton.CFrame = Player.Character.HumanoidRootPart.CFrame
+        task.wait(0.1)
+        v.Buton.CFrame = Player.Character.HumanoidRootPart.CFrame * CFrame.new(-1000, -1000, 10000000)
+        end
+    end
+end)
+
+Main:CreateToggle("Auto Rebirth", true, function()
+    RS.Event.rebirth:FireServer()
+end)
+
+
+
+
+
+
+
+
+-- Teleport
+
+
+
+
+-- Eggs
+
+
+
+
+
+
+
+-- Misc
+
+
+Misc:CreateButton("FPS Boost", function()
+	local decalsyeeted = true
+	local g = game
+	T.WaterWaveSize = 0
+	T.WaterWaveSpeed = 0
+	T.WaterReflectance = 0
+	T.WaterTransparency = 0
+	LI.GlobalShadows = false
+	LI.FogEnd = 9e9
+	LI.Brightness = 0
+	settings().Rendering.QualityLevel = "Level01"
+	for _, v in pairs(g:GetDescendants()) do
+		if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+		elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+			v.Transparency = 1
+		elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+			v.Lifetime = NumberRange.new(0)
+		elseif v:IsA("Explosion") then
+			v.BlastPressure = 1
+			v.BlastRadius = 1
+		elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") then
+			v.Enabled = false
+		elseif v:IsA("MeshPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+			v.TextureID = 10385902758728957
+		end
+	end
+	for _, e in pairs(LI:GetChildren()) do
+		if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+			e.Enabled = false
+		end
+	end
+end)
+
+local TextLabel = Instance.new("TextLabel")
+
+TextLabel.Parent = game:GetService("CoreGui")["UI V2"]
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.0745672435, 0, 0.0299251862, 0)
+TextLabel.Size = UDim2.new(0, 1216, 0, 50)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "FPS: Loading"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 47.000
+TextLabel.TextWrapped = true
+TextLabel.Name = "FPStext"
+TextLabel.Visible = false
+
+FPSAverage = {}
+local Ticking = false
+local LastTick = tick()
+
+local RunService = game:GetService("RunService")
+RunService:BindToRenderStep("UpdateInfo", 1000, function()
+	if Ticking == true then
+		local fps = math.floor(.5 + (1 / (tick() - LastTick)))
+		local sum = 0
+		local ave = 0
+		table.insert(FPSAverage, fps)
+		for i = 1, #FPSAverage do
+			sum = sum + FPSAverage[i]
+		end
+		game:GetService("CoreGui")["UI V2"].FPStext.Text = "FPS: " .. tostring(fps) .. " Average: " .. string.format("%.0f", (sum / #FPSAverage))
+		if (tick() - LastTick) >= 15 then
+			FPSAverage = { }
+			LastTick = tick()
+		end
+		LastTick = tick()
+	end
+end)
+
+
+Misc:CreateButton("Redeem Codes", function()
+    for _,v in pairs(Codes) do
+        RS.EnterCode:FireServer(v)
+    end
+end)
+
+Misc:CreateButton("Show/Hide FPS Counter", function()
+	game:GetService("CoreGui")["UI V2"].FPStext.Visible = not game:GetService("CoreGui")["UI V2"].FPStext.Visible
+	Ticking = not Ticking
+end)
+
+
+Misc:CreateSlider("WalkSpeed", {min = 16, max = 200, default = 16}, function(state)
+    Player.Character.Humanoid.WalkSpeed = state
+end)
+
+Misc:CreateButton("Copy Discord Invite", function()
+    setclipboard("https://discord.gg/ups8GeuYAD")
+end)
+
+local TeleportService = game:GetService("TeleportService")
+
+Misc:CreateButton("Uninject", function()
+    game:GetService("CoreGui")["UI V2"]:Destroy()
+end)
+
+Misc:CreateButton("Uninject and Rejoin", function()
+    game:GetService("CoreGui")["UI V2"]:Destroy()
+    TeleportService:Teleport(game.PlaceId)
+end)

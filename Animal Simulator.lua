ef0797e43b9cc7490c2d20d795855539cfead5b4
WHYSTRIV3 <117491501+WHYSTRIV3@users.noreply.github.com>
2022-12-19 22:24:12 -0500

    Create Animal Simulator.lua

---
+++ Animal Simulator.lua
@@ -0,0 +1,147 @@
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub/main/V1Library.lua", true))()
+local ui = Library:CreateWindow()
+
+local Main = ui:new("Main")
+local Egg = ui:new("Eggs")
+local tp = ui:new("Teleport")
+local Misc = ui:new("Misc")
+
+local VU = game:GetService("VirtualUser")
+local Player = game:GetService("Players").LocalPlayer
+local WS = game:GetService("Workspace")
+local RS = game:GetService("ReplicatedStorage")
+local T = WS.Terrain
+local LI = game:GetService("Lighting")
+local Codes = {"Release", "Alien", "500Likes", "3kLikes", "BeastPet349", "BeastPet491", "BeastPet550"}
+
+
+--Variables
+
+local TeleportWait = false
+
+
+
+
+
+-- Anti Afk
+
+Player.Idled:Connect(function()
+    VU:Button2Down(Vector2.new(0, 0), WS.CurrentCamera.CFrame)
+    wait(1)
+    VU:Button2Up(Vector2.new(0, 0), WS.CurrentCamera.CFrame)
+end)
+
+
+
+
+
+
+--functions
+
+
+
+
+
+
+
+
+
+
+
+--Main
+
+
+
+Main:CreateToggle("Auto Collect Coins", true, function()
+    for i, v in pairs(game:GetService("Workspace").CoinContainer:GetDescendants()) do
+        if v:FindFirstChild("TouchInterest") then
+        firetouchinterest(v, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+        firetouchinterest(v, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+         end
+     end
+end)
+
+
+
+
+
+-- Teleport
+
+
+
+
+
+
+
+
+
+
+
+-- Eggs
+
+
+
+
+
+
+
+
+
+
+
+-- Misc
+
+
+Misc:CreateButton("FPS Boost", function()
+	local decalsyeeted = true
+	local g = game
+	T.WaterWaveSize = 0
+	T.WaterWaveSpeed = 0
+	T.WaterReflectance = 0
+	T.WaterTransparency = 0
+	LI.GlobalShadows = false
+	LI.FogEnd = 9e9
+	LI.Brightness = 0
+	settings().Rendering.QualityLevel = "Level01"
+	for _, v in pairs(g:GetDescendants()) do
+		if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+		elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+			v.Transparency = 1
+		elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+			v.Lifetime = NumberRange.new(0)
+		elseif v:IsA("Explosion") then
+			v.BlastPressure = 1
+			v.BlastRadius = 1
+		elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") then
+			v.Enabled = false
+		elseif v:IsA("MeshPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+			v.TextureID = 10385902758728957
+		end
+	end
+	for _, e in pairs(LI:GetChildren()) do
+		if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+			e.Enabled = false
+		end
+	end
+end)
+
+Misc:CreateButton("Redeem Codes", function()
+    for _,v in pairs(Codes) do
+        RS.EnterCode:FireServer(v)
+    end
+end)
+
+
+Misc:CreateSlider("WalkSpeed", {min = 16, max = 200, default = 16}, function(state)
+    Player.Character.Humanoid.WalkSpeed = state
+end)
+
+Misc:CreateButton("Copy Discord Invite", function()
+    setclipboard("https://discord.gg/ups8GeuYAD")
+end)
+
